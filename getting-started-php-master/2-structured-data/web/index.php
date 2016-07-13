2670a3389af53a0b884d7f038c0ef655834230a0
Felipe Rigel <feliperigel@gmail.com>
2016-07-13 16:03:30 -0600

    Update index.php

--- getting-started-php-master/2-structured-data/web/index.php
+++ getting-started-php-master/2-structured-data/web/index.php
@@ -16,7 +16,7 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
+die ("si jala");
 /** @var Silex\Application $app */
 $app = require __DIR__ . '/../src/app.php';
 require __DIR__ . '/../src/controllers.php';
